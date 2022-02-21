447e09fd2a53a6bdfd86bc4e706e8aa1c266b7f1
redahm <hammada.reda.dev@gmail.com>
2022-02-21 10:41:23 +0100

    update

--- prototypes/prototype-3/insert.php
+++ prototypes/prototype-3/insert.php
@@ -25,11 +25,13 @@ if(!empty($_POST)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prorotype 3</title>
 </head>
 <body>
     <form method="post">
-        
+        <input type="text" name="frist_name">
+        <input type ="text" name="last_name">
+        <input type="text" name="age">
     </form>
 </body>
 </html>
