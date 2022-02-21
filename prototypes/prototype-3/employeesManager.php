86dde27458ec14d72e7e1cf838b5a818d6714994
redahm <hammada.reda.dev@gmail.com>
2022-02-21 10:14:50 +0100

    update

--- prototypes/prototype-3/employeesManager.php
+++ prototypes/prototype-3/employeesManager.php
@@ -1,7 +1,15 @@
 <?php
 
-class EmployeeManager(){
-    
+class EmployeeManager{
+
+    public function getAllEmployees($connectDB){
+
+        $sqlGetData = 'SELECT ID, firt_name, last_name, age FROM employees';
+        $result = mysql_query($connectDB, $sqlGetData);
+        $data = mysql_fetch_all($result, MYSQLI_ASSOC);
+
+        return $data;
+    }
 }
 
 
