48a63999a67635d58ad887196fdbb6386436f1a1
redahm <hammada.reda.dev@gmail.com>
2022-02-21 10:04:51 +0100

    comment added

--- prototypes/prototype-3/config.php
+++ prototypes/prototype-3/config.php
@@ -2,7 +2,7 @@
 
 $connectDB =  mysql_connect('localhost', 'Reda', '123456', 'prototype3');
 
-
+// check if connection went successfully 
 if($connectDB){
 
     echo " data base connection error " . mysqli_connect_error();
