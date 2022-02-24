d49eee69c813bed8b5911f9282eee9935539ec42
redahm <hammada.reda.dev@gmail.com>
2022-02-24 09:18:03 +0100

    update

---
+++ prototypes/prototype-4/insert.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>insert new employee</title>
+</head>
+<body>
+    <form type="text" name="firstName" required>
+    <form type="text" name="lastName" required>
+    <form type="date" name="birthDay" required>
+    </form>
+</body>
+</html>
