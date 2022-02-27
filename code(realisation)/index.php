5a989129e8acc7e2f5d47b49b5704daa60c888de
reda hammada <hammada.reda.dev@gmail.com>
2022-02-27 20:15:55 +0100

    update

--- code(realisation)/index.php
+++ code(realisation)/index.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'employeesManager.php';
 $employeeManager = new employeesManager();
 $data = $employeeManager->getAllEmployees();
@@ -11,29 +12,37 @@ $data = $employeeManager->getAllEmployees();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <header>
-        <nav>
-            <section class="topNavBar container-fluid bg-light ">
-                <div>
-                    
-                    <img class="w-5" src="./img/logo.png">
+        <nav  class="container-fluids">
+            <div class=>
+                <section class="sideNavBar h-100 ">
 
-                </div>
-                <div>
-                    <input type="search" name="search">
-                    <input class="btn btn-success" type="button" value="search">
-                <div>
-                <div>
-                    <a class="btn btn-success" href="logout.php">log out</a>
-                </div>
-            </section>
-            <section class="sideNavBar">
-            </section>
+                    <a>Table</a>
+                    <a>add  employee</a>     
+
+                </section>
+                <section class="topNavBar  bg-light p1 col-12 row align-items-start">
+                    <div class='col'>
+                        
+                        <img class="img" src="./img/logo.png">
+
+                    </div>
+                    <div class='col'>
+                        <input type="search" name="search">
+                        <input class="btn btn-success" type="button" value="search">
+                    <div>
+                    <div class='col d-inline'>
+                        <a class="btn btn-success" href="logout.php">log out</a>
+                    </div>
+                </section>
             
+            
+            <div>
 
         </nav>
     </header>
--- code(realisation)/login.php
+++ code(realisation)/login.php
@@ -14,31 +14,43 @@ if(isset($_POST['username'], $_POST['password'])){
    
     else{
 
-        echo " username or password wrong";
+       $errorMessage;
     }
 }
 
+if(empty($_POST['username'],$_POST['passowrd'])){
+
+    
+}
+
+
+
  
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
     <title>log in</title>
+
 </head>
-<body>
-    <main clas>
-        <section>
-            <form method="post">
-                <input type="text" name="username">
-                <input type="password" name="password">
-                <input type="submit" value="log in">
-            
+
+<body class = 'bg-light w-100'>
+    <main>
+        <section class='w-50 ms-auto   me-auto  bg-white'>
+            <img src="./img/logo.png" alt="logo">
+            <form method="post border rounded rounded-3 w-25 ">
+                <input class='d-block border rounded border-dark ' type="text" name="username">
+                <input class= 'd-block border rounded border-dark ' type="password" name="password">
+                <input class='d-block btn btn-success' type="submit" value="log in">
+                <p class="text-danger" ><?php echo $errorMessage = "username or password is wrong ";  ?></p>
             </form>
         </section>
     </main>
