98153f32e425087a66d9fbea765b892ba4c77bcb
redahm <hammada.reda.dev@gmail.com>
2022-03-08 16:20:28 +0100

    Edit env database

--- employeesManager.php
+++ employeesManager.php
@@ -6,7 +6,16 @@ class EmployeesManager{
 
         private function getConnect(){
             if(is_null($this->connect)){
-                $this->connect =  mysqli_connect('localhost','Reda','123456','realisation');
+                                //Get Heroku ClearDB connection information
+                $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+                $cleardb_server = $cleardb_url["host"];
+                $cleardb_username = $cleardb_url["user"];
+                $cleardb_password = $cleardb_url["pass"];
+                $cleardb_db = substr($cleardb_url["path"],1);
+                $active_group = 'default';
+                $query_builder = TRUE;
+                // Connect to DB
+                $this->connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
             }
 
             else{
